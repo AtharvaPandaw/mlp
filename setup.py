e3ad9a68e441e5a809db9057e9b08e41d888e9be
Atharva Pandaw <atharvapandaw988@gmail.com>
2025-10-18 16:13:36 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+# responsible for making ml program as packages in pypi (like a library like seaborn, numpy etc)
+
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Atharva',
+author_email='atharvapandaw988@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
